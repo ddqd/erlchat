1f4f0502637dfc70c0e13335afb3dff8a9828d98
Dmitry Gureev <dmi.gureev@gmail.com>
2014-01-05 16:15:35 +0400

    fix create proc

--- apps/erlchatsrv/src/erlchatsrv_sup.erl
+++ apps/erlchatsrv/src/erlchatsrv_sup.erl
@@ -6,24 +6,32 @@
 %% API
 -export([start_link/0]).
 
+-export([start_socket/0]).
+
 %% Supervisor callbacks
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type, Args), {I, {I, start_link, Args}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Type, Args), {I, {I, start_link, Args}, temporary, 1000, Type, [I]}).
 
+-define(TCP_OPTIONS, [binary, {packet, 0}, {reuseaddr, true}, {nodelay, true}, {active, true}]).
 
 %% ===================================================================
 %% API functions
 %% ===================================================================
 
+start_socket() ->
+	supervisor:start_child(?MODULE, []).
+
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+	pg2:create(clients),
+	supervisor:start_link({local, ?MODULE}, ?MODULE, []),
+	start_socket().
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
-init([]) ->
-    {ok, { {one_for_all, 5, 10}, [?CHILD(erlchat_srv, worker, [server_logic])]} }.
-
+init([]) ->	
+	{ok, Socket} = gen_tcp:listen(7000, ?TCP_OPTIONS),
+	{ok, { {simple_one_for_one, 60, 3600}, [?CHILD(server_logic, worker, [Socket])]} }.
--- apps/erlchatsrv/src/server_logic.erl
+++ apps/erlchatsrv/src/server_logic.erl
@@ -8,83 +8,40 @@
  
 -export([handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
  
--record(state, {name, socket, group, answer}).
-
--define(TIMEOUT, 1000).
+-record(state, {socket, group}).
 
 start_link(Socket) ->
-	gen_server:start_link(?MODULE, [Socket], []).
+	gen_server:start_link(?MODULE, Socket, []).
 
-event_loop(State) ->
-	lager:log(info, self(), "client pid ~p", [self()]),
-	    case gen_tcp:recv(State#state.socket, 0) of
-        {ok, Data} ->
-            gen_tcp:send(State#state.socket, Data),
-            event_loop(State);
-        {error, closed} ->
-            ok
-    end.
-
-receive_handler(State) ->
-	Socket = State#state.socket,
-	inet:setopts(Socket, [{active, once}]),
-	receive
-		{tcp, Socket, <<"quit", _/binary>>} ->
-			gen_tcp:close(Socket);
-		{tcp, Socket, Msg} ->
-			gen_tcp:send(Socket, Msg),
-			receive_handler(State)
-	end.
-
-init([Socket]) ->
-	{ok, S} = gen_tcp:accept(Socket),
-	gen_server:cast(erlchat_srv, {accepted, self()}),
-	Res = gen_tcp:controlling_process(S, self()),
-	lager:log(info, self(), "test processs___", [Res]),
-	State = #state{socket = S, group=clients},
+init(Socket) ->
+	State = #state{socket = Socket, group=clients},
+	gen_server:cast(self(), accept),
 	{ok, State}.
 
-
 handle_call(_Request, _From, State) ->
-  Reply = ok,
-  {reply, Reply, State}.
-
-handle_cast(start, State) ->
-	lager:log(info, self(), "start", []),
-	{noreply, State};
-
+	Reply = ok,
+	{reply, Reply, State}.
 
 handle_cast(accept, State) ->
+	{ok, Socket} = gen_tcp:accept(State#state.socket),
+	gen_tcp:controlling_process(Socket, self()),
+	erlchatsrv_sup:start_socket(),
 	{noreply, State};
 
-handle_cast(test, State) ->
-	lager:log(info, self(), "test pooq", []),
-  {noreply, State};
-
 handle_cast(_Msg, State) ->
-  {noreply, State}.
-
-handle_info(start, State) ->
-	% lager:log(info, self(), "test pooq", []),
- % receive_handler(State),
-  {noreply, State};
-
-handle_info(test, State) ->
-	lager:log(info, self(), "test pooq", []),
-  {noreply, State};
+	{noreply, State}.
 
 handle_info(Info, State) ->
-	lager:log(info, self(), "info", [Info]),
-	gen_tcp:controlling_process(State#state.socket, self()),
-  {noreply, State}.
+	lager:log(info, self(), "info ~p, PID ~p", [Info, self()]),
+	{noreply, State}.
   
 stop() ->
-    gen_server:call({local, ?MODULE}, stop).
+	gen_server:call({local, ?MODULE}, stop).
  
 terminate(_Reason, _State) ->
-  ok.
- 
+	ok.
+
 code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+	{ok, State}.
 
 
